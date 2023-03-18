382365aef914af21e6308eb660ef14938cfddf9b
jinu Yi <dionisos198@naver.com>
2023-03-18 16:04:30 +0900

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        System.out.println("a+b = "+ (a+b));
+        System.out.println("FOR GIT PRACTICE");
+    }
+}
