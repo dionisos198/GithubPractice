46eba9a1cf8fb13f791e3e589786e4359f091c44
Jinu Yi <dionisos198@naver.com>
2023-12-02 12:13:03 +0900

    코드에 안녕하세요를 넣었습니다

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println("a+b = "+ (a+b));
         System.out.println("FOR GIT PRACTICE");
         System.out.println("코드를 더 좋게 수정하였습니다");
+        System.out.println("안녕하세요");
     }
 }
